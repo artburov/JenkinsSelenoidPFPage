bae9a433ac8601b263747e52999e927af1bd3ed0
Arthur Burov <art.burov@gmail.com>
2021-04-06 20:05:14 +0300

    Base tests of the Practice Form

---
+++ src/test/java/tests/PracticeFormTests.java
@@ -0,0 +1,46 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pages.PracticeFormPage;
+
+public class PracticeFormTests {
+
+    PracticeFormPage practiceFormPage = new PracticeFormPage();
+
+    @BeforeAll
+    static void setup() {
+        Configuration.browserSize = "1280x1024";
+    }
+
+
+    @Test
+    void successFillForm() {
+        practiceFormPage.openForm()
+                .setName()
+                .setSurname()
+                .setEmail()
+                .setGender()
+                .setPhone()
+                .setBirthDate()
+                .setSubjects()
+                .setHobbies()
+                .imageAdd()
+                .addressSet()
+                .userState()
+                .userCity()
+                .formSubmit()
+                .modalTitleAssert()
+                .modalNameSurnameAssert()
+                .modalEmailAssert()
+                .modalGenderAssert()
+                .modalPhoneAssert()
+                .modalUserBirthDayAssert()
+                .modalUserSubjectAssert()
+                .modalUserHobbiesAssert()
+                .modalUserImageAssert()
+                .modalUserAddress()
+                .modalStateCityAssert();
+    }
+}
